7ee01dc43b6d5695ea93bd2ea10ae32fc96c1060
S.Muresan <s.muresan@spgs.org>
2026-01-25 17:16:45 +0000

    extra stuff and streamlining

---
+++ graphdata.py
@@ -0,0 +1,71 @@
+graph_data = {
+    'number-theory': {
+        'title': 'Number Theory',
+        'color': '#3d1a10',  # Deep Red/Brown (Algebra-esque)
+        'posts': [
+            {'id': 1, 'label': 'Primes', 'img': '/static/primes.jpg', 'intro': 'The building blocks of arithmetic.', 'url': '/primes'},
+            {'id': 2, 'label': 'Modular', 'img': '/static/mod.jpg', 'intro': 'Clock arithmetic and congruences.', 'url': '/modular'},
+            {'id': 3, 'label': 'Riemann', 'img': '/static/zeta.jpg', 'intro': 'The most famous unsolved hypothesis.', 'url': '/riemann'}
+        ]
+    },
+    'space': {
+        'title': 'Space',
+        'color': '#0d1b2a',  # Midnight Navy
+        'posts': [
+            {'id': 1, 'label': 'Black Holes', 'img': '/static/bh.jpg', 'intro': 'Singularities in the fabric of spacetime.', 'url': '/black-holes'},
+            {'id': 2, 'label': 'Nebulae', 'img': '/static/nebula.jpg', 'intro': 'Stellar nurseries of gas and dust.', 'url': '/nebulae'}
+        ]
+    },
+    'topology': {
+        'title': 'Topology',
+        'color': '#2d3e50',  # Slate Blue/Grey
+        'posts': [
+            {'id': 1, 'label': 'Manifolds', 'img': '/static/man.jpg', 'intro': 'Spaces that look flat up close.', 'url': '/manifolds'},
+            {'id': 2, 'label': 'Donuts', 'img': '/static/torus.jpg', 'intro': 'Why coffee cups are just like donuts.', 'url': '/torus'}
+        ]
+    },
+    'history': {
+        'title': 'History',
+        'color': '#4a3728',  # Aged Parchment/Sepia
+        'posts': [
+            {'id': 1, 'label': 'Industrial', 'img': '/static/rev.jpg', 'intro': 'The era of steam and steel.', 'url': '/industrial'}
+        ]
+    },
+    'material-science': {
+        'title': 'Material Science',
+        'color': '#1b4332',  # Deep Forest Green
+        'posts': [
+            {'id': 1, 'label': 'Polymers', 'img': '/static/poly.jpg', 'intro': 'Long chains of repeating molecules.', 'url': '/polymers'},
+            {'id': 2, 'label': 'Graphene', 'img': '/static/graph.jpg', 'intro': 'A single layer of carbon atoms.', 'url': '/graphene'}
+        ]
+    },
+    'mechanics': {
+        'title': 'Mechanics',
+        'color': '#582f0e',  # Rust/Ochre
+        'posts': [
+            {'id': 1, 'label': 'Dynamics', 'img': '/static/dyn.jpg', 'intro': 'The study of bodies in motion.', 'url': '/dynamics'}
+        ]
+    },
+    'cows': {
+        'title': 'Cows',
+        'color': '#111d13',  # Dark Moss
+        'posts': [
+            {'id': 1, 'label': 'Bovines', 'img': '/static/cow1.jpg', 'intro': 'Exploring the biology of cattle.', 'url': '/bovines'}
+        ]
+    },
+    'recommendations': {
+        'title': 'Recommendations',
+        'color': '#280f34',  # Deep Plum/Other
+        'posts': [
+            {'id': 1, 'label': 'Books', 'img': '/static/books.jpg', 'intro': 'Essential reads for the curious.', 'url': '/books'}
+        ]
+    },
+    'quantum': {
+        'title': 'Quantum',
+        'color': '#002855',  # Deep Electric Blue
+        'posts': [
+            {'id': 1, 'label': 'Entanglement', 'img': '/static/ent.jpg', 'intro': 'Spooky action at a distance.', 'url': '/entanglement'},
+            {'id': 2, 'label': 'Photons', 'img': '/static/light.jpg', 'intro': 'Particles and waves of light.', 'url': '/photons'}
+        ]
+    }
+}
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, flash
 import smtplib
 from email.message import EmailMessage
+from graphdata import graph_data
 import os
 
 app = Flask(__name__)
@@ -19,25 +20,37 @@ def index():
 def sitemaps():
     return render_template('sitemaps.xml')
 
+
 @app.route('/posts')
 def posts():
-    return render_template('posts.html')
+    return render_template('posts.html', graph_data=graph_data)
+
+
+@app.route('/posts/<category>')
+def show_graph(category):
+    data = graph_data.get(category)
+    return render_template('graph_page.html', hub_title=data['title'], hub_color=data['color'], posts=data['posts'])
+
 
 @app.route('/subscribe')
 def subscribe():
     return render_template('subscribe.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/memes')
 def memes():
     return render_template('memes.html')
 
+
 @app.route('/olympiad')
 def olympiad():
     return render_template('olympiad.html')
+
 
 # This bit is for the contact page if they want to contact us...
 # @app.route('/contact', methods=['GET', 'POST'])
